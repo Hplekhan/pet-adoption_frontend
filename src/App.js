ac82b134e4add81ffafc7c1c956cf6337cbc3b6b
Robin George <119731003+robinrgc@users.noreply.github.com>
2024-04-25 14:48:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import './App.css';
 import Aboutus from './components/Aboutus';
 import Menuu from './components/Menuu';
@@ -14,18 +14,4 @@ function App() {
 
   );
 }
-=======
-import Home from './HomePage/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-function App() {
-  return (
-   <>
-    <Home/>
-   </>
-  );
-}
-
->>>>>>> 9567d718224360b11324b98d34b8ef222d7eaf3d
 export default App;
